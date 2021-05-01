e59ed205e2b909b0346b7700a5e0d865f0c1dcec
katexue <kate.xue.work@gmail.com>
2021-05-01 18:00:34 +1000

    updated episodes to store module.

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,37 @@
 import { createStore, createLogger } from 'vuex'
 import characters from './modules/characters'
 import character from './modules/character'
-// import episode from './modules/episode'
+import episodes from './modules/episodes'
 // import modal from './modules/modal'
 
 const modules = {
   characters,
-  character
-  // episode,
+  character,
+  episodes
   // modal
 }
 
 // Separate STATE function for easier default state value reset.
-const STATE = () => {}
+// const STATE = () => {}
 
-const state = STATE()
+// const state = STATE()
 
-const actions = {}
+// const actions = {}
 
-const mutations = {}
+// const mutations = {}
 
-const getters = {}
+// const getters = {}
 
 // Add logger for easier debug as Vue.js devtool doesn't support vuex for vue3 atm (sad...)
 const plugins = process.env.NODE_ENV === 'development' ? [createLogger()] : []
 
 const store = createStore({
   plugins,
-  modules,
-  actions,
-  mutations,
-  getters,
-  state
+  modules
+  // actions,
+  // mutations,
+  // getters,
+  // state
 })
 
 export default store
